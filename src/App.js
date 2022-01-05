4722160d8ebc1823e6f53f240fb89346df0b4601
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-05 12:36:18 +0200

    adds refactoring App with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.production.min";
+import { useState,useEffect } from 'react';
 import Searchbar from "./Searchbar/Searchbar";
 import { ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,66 +10,56 @@ import Loading from "./plugins/loading";
 import Modal from "./Modal/Modal";
 
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    searchValue: '',
-    arrayOfImages: [],
-    page: 1,
-    visible: false,
-    showModal: false,
-    modalImage: null,
-    modalTag:null
-  };
+  const [searchValue, setSearchValue] = useState('');
+  const [arrayOfImages, setArrayOfImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [visible, setVisible] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  const [modalTag, setModalTag] = useState(null);
+  
+  useEffect(() => {
+    if (!searchValue) {
+      return
+    }
+    fetch()
+  }, [searchValue, page]);
 
-componentDidUpdate(prevProps, prevState) {
-
-      if (prevState.searchValue !== this.state.searchValue) {
-      this.setState({ page: 1, arrayOfImages: [], visible: true })
-        this.fetch()   
-   };
-
-      if (prevState.searchValue === this.state.searchValue && prevState.page !== this.state.page) {
-        this.fetch()
-      }    
-      };
-
-
-  async fetch() {
-    const{searchValue, page, arrayOfImages} = this.state
+  const fetch = async () => {
     try {
       Loading.circle()
       const response = await axios.get(`https://pixabay.com/api/?q=${searchValue}&page=${page}&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
       const pictures = await response.data
         
       if (pictures.totalHits === 0) {
-        this.setState({ visible: false})
+        setVisible(false)
         toast.info('No pictures found for your request')
         Loading.remove()
         return
-      }
-      if (page === 1) {
-        this.setState({
-          arrayOfImages: [...pictures.hits]
-        })
-      }
-    
+      };
 
+      if (page === 1) {
+        setArrayOfImages([...pictures.hits])
+      };
+
+  
       if (arrayOfImages.length === pictures.totalHits) {
-        this.setState({ visible: false})
+        setVisible(false)
       };
 
       if (page > 1) {
-        this.setState(prevState => ({
-        arrayOfImages: [...prevState.arrayOfImages, ...pictures.hits],
-        }));
-        
+        setArrayOfImages(prev => 
+          [...prev, ...pictures.hits]
+        )
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: "smooth",
         });
-      }
+      };
     }
+
     catch (error) {
       console.log(error);
       return Promise.reject(error);
@@ -77,46 +67,45 @@ componentDidUpdate(prevProps, prevState) {
     Loading.remove()
   };
 
-  
-  handleClickLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }))
+ const getImageforModal = (image, tag) => {
+   toggleModal()
+   setModalImage(image);
+   setModalTag(tag);
   };
 
-  handleFormSubmit = value => {
-    this.setState({ searchValue: value })
+  const handleClickLoadMore = () => {
+      setPage(prev => prev + 1)
   };
 
- 
-  toggleModal = () => {
-    this.setState(({showModal}) =>({ showModal: !showModal })
-    )
-  }
-  getImageforModal = (image, tag) => {
-    this.toggleModal()
-    this.setState({ modalImage: image, modalTag: tag });
+ const handleFormSubmit = value => {
+   setSearchValue(value)
+   setPage(1)
+   setArrayOfImages([])
+   setVisible(true)
   };
 
-  render() {
-    const {arrayOfImages, visible, showModal,modalImage,modalTag} = this.state
-    return (
-      <>
-        <ToastContainer />
-        <Searchbar formSubmit={this.handleFormSubmit} />
-        <ImageGallery >
-          <ImageGalleryItem images={arrayOfImages}
-            onGetImage={this.getImageforModal}
+  const toggleModal = () => {
+    setShowModal(!showModal)
+  };
 
-          />
-        </ImageGallery>
-        {visible && <Button
-          onHandleClickLoadMore={this.handleClickLoadMore} />}
-        {showModal &&
-          <Modal onClose={this.toggleModal}>
-            <img src={modalImage} alt={modalTag} />
-          </Modal>}
-      </>
-    )
-  }
-}
+  return (
+    <>
+      <ToastContainer />
+      <Searchbar formSubmit={handleFormSubmit} />
+      <ImageGallery >
+        <ImageGalleryItem images={arrayOfImages}
+          onGetImage={getImageforModal}
+
+        />
+      </ImageGallery>
+      {visible && <Button
+        onHandleClickLoadMore={handleClickLoadMore} />}
+      {showModal &&
+        <Modal onClose={toggleModal}>
+          <img src={modalImage} alt={modalTag} />
+        </Modal>}
+    </>
+  );
+};
 
 export default App;
